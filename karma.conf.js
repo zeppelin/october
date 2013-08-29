ccaa3b3f1bc26a0849434ab5922b0e51a096c312
Adam Haris <iamadamharis@gmail.com>
2013-08-29 19:12:39 +0200

    ember model added

--- karma.conf.js
+++ karma.conf.js
@@ -13,6 +13,7 @@ module.exports = function(config) {
       'vendor/jquery/jquery.js',
       'vendor/handlebars/handlebars.js',
       'vendor/ember/index.js',
+      'vendor/ember-model/ember-model.js',
       'assets/templates.js',
       'assets/app.js',
       'tests/test_helper.js',
