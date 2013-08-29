6293f6f7c2293dd005849315ada1fafb4123adfa
Balint Erdi <balint.erdi@gmail.com>
2013-08-29 15:25:15 +0200

    Fix redirection in TokenRoute

--- app/routes/token.js
+++ app/routes/token.js
@@ -2,7 +2,7 @@ var TokenRoute = Ember.Route.extend({
   model: function(params) {
     if (params.token) {
       this.controllerFor('application').set('token', params.token);
-      this.redirectTo('repo');
+      this.transitionTo('repo');
     }
   }
 });
